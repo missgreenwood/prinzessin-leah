f119271d03639a2a586b65c65d608984982f976d
Sascha Goldhofer <post@saschagoldhofer.de>
2014-04-19 19:12:23 +0200

    Update template markup

--- page-templates/links.php
+++ page-templates/links.php
@@ -6,31 +6,31 @@
 
    <?php get_header(); ?>
 
-      <div id="articles" class="columns"><div class="columns-wrapper">
-
+      <article>
 
    		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
    			<div class="entry">
-   				<?php 
+   				<?php
    				if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
    					the_post_thumbnail();
-   					} 
+   					}
    				?>
    			   	<?php the_content(); ?>
       			<ul>
          			<?php wp_list_bookmarks("categorize=&title_li="); ?>
          		</ul>
      		</div>
-     	<?php endwhile; endif; ?> 
-   
 
-   </div></div>
-   
-      </div><!-- content -->
-   
-      <?php get_sidebar(); ?>
+     	<?php endwhile; endif; ?>
 
-   </div><!-- website -->
 
-   <!-- include footer.php -->
-   <?php get_footer(); ?>
+      </article>
+
+   </div><!-- content -->
+
+    <?php get_sidebar(); ?>
+
+</div><!-- website -->
+
+<!-- include footer.php -->
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,6 +1,10 @@
 		<?php get_header(); ?>
 
+			<!--
 
+			SINGLE PAGE
+
+			-->
 
 			<article>
 
