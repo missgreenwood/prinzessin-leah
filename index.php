52d0d567c3608edd50794436516deaa92901afbf
Sascha Goldhofer <post@saschagoldhofer.de>
2014-04-19 20:50:49 +0200

    Refactor replace markup with macro

--- index.php
+++ index.php
@@ -1,53 +1,66 @@
 	<!-- include header.php -->
 	<?php get_header(); ?>
 
+	<?
+
+		$COLUMNS_START = '<div class="columns row"><div class="columns-wrapper">'.PHP_EOL;
+		$COLUMNS_END = '</div></div>'.PHP_EOL;
+
+		$COLUMN_LEFT = '<div class="left column">'.PHP_EOL;
+		$COLUMN_RIGHT = '<div class="right column">'.PHP_EOL;
+		$COLUMN_END = '</div>'.PHP_EOL;
+
+		/*
+
+		- benötigte Anzahl an Posts laden (~"post count")
+			-> Anzahl = 1(Hauptartikel) + 2 * <rowcount>(Mitte) + <rowcount> + 1(Seitenspalte, neben Haupt)
+			=> Anzahl = 2 + 3*<rowcount>
+
+		A. Alle posts laden, dann einige in Hauptspalte, einige in Seitenspalte ausgeben
+			- while loop macht das nicht mit => for - loop?
+
+
+			get_posts() parameters:
+			array(
+				'posts_per_page'   => 5,
+				'offset'           => 0,
+				'category'         => '',
+				'orderby'          => 'post_date',
+				'order'            => 'DESC',
+				'include'          => '',
+				'exclude'          => '',
+				'meta_key'         => '',
+				'meta_value'       => '',
+				'post_type'        => 'post',
+				'post_mime_type'   => '',
+				'post_parent'      => '',
+				'post_status'      => 'publish',
+				'suppress_filters' => true ); ?>
+		*/
+
+		// In Settings: 8 posts
+		// $posts = get_posts(array(
+		// 	'posts_per_page'   => 5,
+		// ));
+		// $count = count($posts);
+		// $current_post_index = 0;
+
+		$count = $wp_query->post_count;
+
+		echo "<!--";
+		var_dump();
+		echo "-->";
+	?>
+
+
 	<div id="articles" class="columns"><div class="columns-wrapper">
-
-			<?php
-				/*
-
-				- benötigte Anzahl an Posts laden (~"post count")
-					-> Anzahl = 1(Hauptartikel) + 2 * <rowcount>(Mitte) + <rowcount> + 1(Seitenspalte, neben Haupt)
-					=> Anzahl = 2 + 3*<rowcount> 
-				
-				A. Alle posts laden, dann einige in Hauptspalte, einige in Seitenspalte ausgeben
-					- while loop macht das nicht mit => for - loop?
-
-					
-					get_posts() parameters:
-					array(
-						'posts_per_page'   => 5,
-						'offset'           => 0,
-						'category'         => '',
-						'orderby'          => 'post_date',
-						'order'            => 'DESC',
-						'include'          => '',
-						'exclude'          => '',
-						'meta_key'         => '',
-						'meta_value'       => '',
-						'post_type'        => 'post',
-						'post_mime_type'   => '',
-						'post_parent'      => '',
-						'post_status'      => 'publish',
-						'suppress_filters' => true ); ?>
-				*/
-
-				// In Settings: 8 posts
-				// $posts = get_posts(array(
-				// 	'posts_per_page'   => 5,
-				// ));
-				// $count = count($posts);
-				// $current_post_index = 0;
-			?>
-
-			<h1><?php echo $count; ?></h1>
 
 			<!-- HAUPTSPALTE -->
 			<div id="column-main" class="left column">
 
 				<!-- ERSTER ARTIKEL -->
 				<div id="latest">
-					
+
 					<?php
 						// Lade den nächsten Post als globales Objekt
 						the_post();
@@ -60,47 +73,34 @@
 				</div>
 				<!-- ENDE ERSTER ARTIKEL -->
 
-
 				<?php
-					
-					// PHP SCHLEIFE FÜR RESTLICHE POSTS
-				?>
 
-				<!-- MITTE -->
-				<div class="columns row"><div class="columns-wrapper">
+					/*
+						CENTER
+					 */
 
-					<!-- RECHTE SPALTE -->
-					<div class="left column">
-						
-						<?php
+					echo $COLUMNS_START;
+
+						echo $COLUMN_LEFT;
+
 							the_post();
 							$current_post_index += 1;
 							print_post_teaser($post);
-						?>
 
-					</div>
+						echo $COLUMN_END;
 
 
-					<!-- RECHTE SPALTE -->
-					<div class="right column">
-						
-						<?php
+						echo $COLUMN_RIGHT;
+
 							the_post();
 							$current_post_index += 1;
 							print_post_teaser($post);
-						?>
 
-					</div>
+						echo $COLUMN_END;
 
+					echo $COLUMNS_END;
 
-				</div></div>
-				<!-- ENDE MITTE -->
-
-				<?php
-					
-					// ENDE PHP SCHLEIFE
 				?>
-	
 
 			</div>
 			<!-- ENDE HAUPTSPALTE -->
@@ -110,43 +110,34 @@
 			<div id="column-side" class="right column">
 
 				<?php
-					
-					// PHP SCHLEIFE FÜR RESTLICHE POSTS
-				?>
 
-				
-				<!-- REIHE -->
-				<div class="columns"><div class="columns-wrapper">
-					
+					/*
 
-					<!-- LINKE SPALTE -->
-					<div class="left column">
-						
-						<?php
+						SIDE
+
+					*/
+
+					echo $COLUMNS_START;
+
+						echo $COLUMN_LEFT;
+
 							the_post();
 							$current_post_index += 1;
 							print_post_teaser($post);
-						?>
 
-					</div>
-					
+						echo $COLUMN_END;
 
-					<!-- RECHTE SPALTE -->
-					<div class="right column">
-						
-						<?php
+
+						echo $COLUMN_RIGHT;
+
 							the_post();
 							$current_post_index += 1;
 							print_post_teaser($post);
-						?>
 
-					</div>
+						echo $COLUMN_END;
 
-				</div></div>
+					echo $COLUMNS_END;
 
-				<?php
-					
-					// PHP SCHLEIFE ENDE
 				?>
 
 			</div>
@@ -156,7 +147,7 @@
 	</div></div><!-- articles -->
 
 	</div><!-- content -->
-	
+
 <?php get_sidebar(); ?>
 
 </div><!-- website -->
