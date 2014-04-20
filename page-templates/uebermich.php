c5e1c131376c7c1354846d1d04b33d647820258f
judith <obermaedchen@gmail.com>
2014-04-20 20:35:20 +0200

    Add first version portrait page template

---
+++ page-templates/uebermich.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Über mich-Page
+ */
+?>
+   <?php get_header(); ?>
+        
+
+        <article>
+            
+            <?php 
+                the_post();
+            ?>
+
+            
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/uebermich.png"/>
+            
+            <div class="portrait"> <?php the_content(); ?> </div>
+            
+        </article>  
+
+
+    </div><!-- content -->
+
+  <?php get_sidebar(); ?>
+
+</div><!-- website -->
+
+<!-- include footer.php -->
+<?php get_footer(); ?>
