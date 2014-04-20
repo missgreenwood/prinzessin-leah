d992b2c458f1d6e97b9164131f9f64b98821334d
Sascha Goldhofer <post@saschagoldhofer.de>
2014-04-20 21:18:38 +0200

    Update page navigation styles

--- archive.php
+++ archive.php
@@ -1,76 +1,88 @@
-	<?php 
-	
+	<?php
+
 	get_header(); ?>
 
-		
-	
+			<?php
+				// show as teaser
+				$more = 0;
+				// current page
+				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+				// options: posts per page
+				$posts_per_page = $wpdb->get_var("SELECT option_value FROM $wpdb->options WHERE option_name = 'posts_per_page'");
 
-				<?php
-					
-					// 1. load all posts 
+				// posts of current category
+				$posts_with_category = $all_posts;
 
-					//$args = array( 'offset' => 5, 'numberposts' => -1, 'posts_per_page' => 9);
-					//$myposts = get_posts( $args );
-					
+				// If no category is selected, strip start-page number of items from overview
+				$page_offset = $posts_per_page * $paged;
 
-					// echo "<h1>archive ".$cat."</h1>";
-					
-					
-					// show as teaser
-					$more = 0;
-					// current page
-					$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-					// options: posts per page
-					$posts_per_page = $wpdb->get_var("SELECT option_value FROM $wpdb->options WHERE option_name = 'posts_per_page'");
-					// build posts query
-					$wp_query = new WP_Query(array(
 
-						"offset" => $posts_per_page * $paged,
-						"cat" => $cat,
+				// else determine posts of current category listed on start page
+				if ($cat !== 28) {
+
+					$page_offset = 0;
+					$posts_array = get_posts(array(
 						"posts_per_page" => $posts_per_page
 					));
 
-					include("post_wall.php");
-					
-				?>
-					
-					<div id="page-navigation">
-						<div id="prev"><?php previous_posts_link("neuere Beiträge"); ?></div>
-						<div id="pagenumber">Seite <?php echo $paged; ?></div>
-						<div id="next"><?php if ($wp_query->post_count == $posts_per_page) next_posts_link("frühere Beiträge"); ?></div>
-					</div>
+					// scan all categories for each post on start page, if it matches
+					// selected category. if yes, increase $page_offset, skipping this post
+					for ($i = 0; $i < count($posts_array); $i++) {
 
-				<?php
+						// categories of current post
+						$categories = get_the_category($posts_array[$i]->ID);
 
-					
-					// 2. 5 Posts ausgeben, starte mit Post no 6
+						for ($c = 0; $c < count($categories); $c++) {
 
-					// global $query_string;				
-					// query_posts( $query_string . '&posts_per_page=9' );
+							if ($categories[$c]->term_id === $cat) {
+								$page_offset += 1;
+								break;
+							}
+						}
+					}
 
-					/* 3. Blaetterfunktion
-						-> Direkt for loop einfuegen:
-						
-						global $query_string;				
-						query_posts( $query_string . '&posts_per_page=9' );
+					// Updated total number of posts
+					$category = get_category($cat);
+					$categories_posts = $category->category_count;
 
-						-> nach den ersten 5 Posts: 
-						next_posts_link();
-						
- 				*/
-				?>
+				} else {
 
-				<?php
-				/* Shows up to 8 posts, excluding the first 5 
-				if ( have_posts() ) : query_posts('showposts=8&offset=6'); while ( have_posts() ) : the_post();
-				endwhile; endif; 
-				*/
-				?>
-				
-		
+					// number of all posts in archive
+					$archived_posts = wp_count_posts()->publish;
+					$categories_posts = $archived_posts;
+				}
+
+				// pages to show
+				$pages_count = ceil(($categories_posts - $page_offset)/$posts_per_page);
+
+				// finally, build posts query
+				$wp_query = new WP_Query(array(
+
+					"offset" => $page_offset + $posts_per_page * ($paged - 1),
+					"cat" => $cat,
+					"posts_per_page" => $posts_per_page
+				));
+
+				// echo "<h1>cat: ". $cat. ' offset: ' . $page_offset . " all posts " . $all_posts . " loaded posts:" . $wp_query->post_count . "</h1>";
+				// echo "<h2>Seite" . $paged . '/' . $pages_count . "</h2>";
+
+
+				include("post_wall.php");
+
+			?>
+
+			<div class="content">
+
+				<div id="page-navigation">
+					<div id="prev" class="button"><?php previous_posts_link("&laquo; neuere Beiträge"); ?></div>
+					<div id="pagenumber"><?php echo $paged . '|' . $pages_count; ?></div>
+					<div id="next" class="button"><?php if ($pages_count !== $paged) next_posts_link("frühere Beiträge &raquo;"); ?></div>
+				</div>
+
+			</div>
 
 		</div><!-- content -->
-		
+
 	<?php get_sidebar(); ?>
 
 	</div><!-- website -->
