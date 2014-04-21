784a351e668845d9c31a512daafb9bf192f91c17
Sascha Goldhofer <post@saschagoldhofer.de>
2014-04-21 15:05:38 +0200

    Change page markup to article

--- page.php
+++ page.php
@@ -1,23 +1,23 @@
 <?php get_header(); ?>
 
-   <div id="articles" class="columns"><div class="columns-wrapper">
- 
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-         <h3><?php the_title(); ?></a></h3>
-         <div class="entry">
-         	<?php 
-         	if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-         		the_post_thumbnail();
-         		} 
-         	?>
-            <?php the_content(); ?>
-         </div>
-      <?php endwhile; endif; ?>
- 
-   </div></div>
- 
+         <article>
+
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+               <h3><?php the_title(); ?></a></h3>
+               <div class="entry">
+               	<?php
+               	if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+               		the_post_thumbnail();
+               		}
+               	?>
+                  <?php the_content(); ?>
+               </div>
+            <?php endwhile; endif; ?>
+
+         </article>
+
       </div><!-- content -->
-   
+
       <?php get_sidebar(); ?>
 
    </div><!-- website -->
