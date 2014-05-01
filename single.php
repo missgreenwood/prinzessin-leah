ca3153446eeb43eff74bfc4b438fdaeb33286023
judith <obermaedchen@gmail.com>
2014-05-01 13:21:47 +0200

    Remove featured images from single posts

--- single.php
+++ single.php
@@ -68,10 +68,10 @@
 					<h1><?php the_title(); ?></h1>
 
 				<?php
-					if (has_post_thumbnail()) {
+					/* if (has_post_thumbnail()) {
 						// check if the post has a Post Thumbnail assigned to it.
 						the_post_thumbnail();
-					}
+					} */
 				?>
 
 				<?php
